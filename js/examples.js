446e3cd4773f0131419953e60d2304d9d5bcc43d
Tobias Mansfield-Williams <contact@tmwhere.com>
2021-04-30 13:50:25 +0200

    Add blocks.throne without drawing

--- js/examples.js
+++ js/examples.js
@@ -1,4 +1,5 @@
 import family from "../examples/family.throne";
 import guessingGame from "../examples/guessing-game.throne";
 import gameOfLife from "../examples/game-of-life.throne";
-export { family, guessingGame, gameOfLife };
+import blocks from "../examples/blocks.throne";
+export { family, guessingGame, gameOfLife, blocks };
